f8ab3a2b3bf3aa928407c72aacaec2c2892192c5
lufepama <lufepama31@gmail.com>
2021-12-12 03:49:57 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Martinita from '../public/martina.JPG'
+import styles from '../styles.module.css'
 
 
 export default function Home({ user }) {
@@ -44,34 +45,38 @@ export default function Home({ user }) {
   }, [])
 
   return (
-    <div className="container">
+    <div className="container ">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
+      <main className={styles.main} >
+        <h1 className={styles.title}>
           Holiii! Escribe aqui abajo el nombre que te ha tocado en el sorteo!
         </h1>
-        <h2>ASI SABREMOS QUIEN FALTA</h2>
-        <h5>Martinita os ama</h5>
-
+        <h2 className={styles.title}>ASI SABREMOS QUIEN FALTA</h2>
+        <h5 className={styles.title}>(Martinita os ama)</h5>
 
         {
           list.map((item) => (
-            <li key={item}>{item}</li>
+            <li key={item} className={styles.items} >{item}</li>
           ))
         }
 
-        <form>
-          <input type='text' name='name' onChange={handleChange('name')} />
-          <button onClick={addUser} disabled={isDisable} >Enviar</button>
+        <form className={styles.formulario}>
+          <input type='text' name='name' onChange={handleChange('name')} className={styles.input} />
+          <button className={styles.button} onClick={addUser} disabled={isDisable} >Enviar</button>
         </form>
+        {
+          isDisable && (
+            <h1 className={styles.message}>Gracias, y que viva Colombia!</h1>
+          )
+        }
         <Image
           src={Martinita}
-          width={250}
-          height={250}
+          width={350}
+          height={350}
           alt='Martinita'
           objectFit='contain'
         />
